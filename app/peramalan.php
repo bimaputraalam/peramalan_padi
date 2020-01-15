4831f46ed380190424b5d49ee689c2d5111707c3
bimaputraalam <bimaputraalam88@gmail.com>
2020-01-15 14:01:08 +0700

    clear

--- app/peramalan.php
+++ app/peramalan.php
@@ -30,10 +30,7 @@ include '../templating/navigation_bar.php';
 <div class="row">
 	<div class="col-12">
 		<div class="card-box">
-			<h4 class="m-t-0 header-title">Default Example</h4>
-			<p class="text-muted font-14 m-b-30">
-				DataTables has most features enabled by default, so all you need to do to use it with your own tables is to call the construction function: <code>$().DataTable();</code>.
-			</p>
+			
 			
 			<table id="datatable1" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
 				<thead>
